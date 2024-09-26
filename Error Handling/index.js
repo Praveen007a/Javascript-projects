734d8be8585b1160be08cc5c3df9feb7980ff66c
PRAVEEN887 <praveendhamodharncs2020@gmail.com>
2024-09-26 23:36:35 +0530

    Update index.js

--- Error Handling/index.js
+++ Error Handling/index.js
@@ -12,3 +12,36 @@ finally{
     console.log("bye...")
 }
 
+//json
+/* 
+    store any data types like String,number,char,boolean, arrays, obects,arrayof objects etc...
+    but if any str value is present must denotes in (" ") otherwise you enter manually...
+*/
+
+let json1="Praveen";
+let json2= 97;
+
+let json3 = `[
+    {
+        "name":"Praveen",
+        "age":22
+    },
+    {
+        "name":"Aruna",
+        "age":21
+    },
+    {
+        "name":"Sahil",
+        "age":23
+    }
+];`
+
+//parse
+let parsed=JSON.parse(json3);
+console.log(parsed);
+//stringify
+
+//let srt=JSON.stringify(json3);
+
+
+//console.log(str);
